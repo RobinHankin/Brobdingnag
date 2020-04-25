4b5e51f15a7fab91685355cb4306699b1d425570
RobinHankin <hankin.robin@gmail.com>
2020-04-25 16:35:41 +1200

    more work on S4

--- R/matrix.R
+++ R/matrix.R
@@ -15,9 +15,14 @@ setValidity("brobmat", .Brob.valid)
     new("brobmat", x=x, positive=c(positive)) # this is the only use of new() here
 }
 
-`brobmat` <- function(..., positive){
-    if(is.matrix(list(...)[[1]])){return(as.brobmat(list(...)[[1]]))}
-    M <- matrix(...)
+`brobmat` <- function(data = NA, nrow = 1, ncol = 1, byrow = FALSE, dimnames = NULL,positive){
+    if(is.matrix(data)){
+        M <- data
+    } else if(is.brob(data)){
+        return(newbrobmat(x=matrix(getX(data),nrow=length(data),ncol=ncol,byrow=byrow,dimnames=dimnames), positive=c(getP(data))))
+    } else { 
+        M <- matrix(data = data, nrow = nrow, ncol = ncol, byrow = byrow, dimnames = dimnames)
+    }
     if(missing(positive)){positive <- rep(TRUE,length(M))}
     positive <- cbind(c(M),positive)[,2]>0
     return(newbrobmat(M,positive=positive))
@@ -35,6 +40,7 @@ setMethod("getP","brobmat",function(x){
     return(out)
 })
 setMethod("getP","numeric",function(x){x>0})
+setMethod("length","brobmat",function(x){length(getX(x))})
 
 `as.brobmat` <- function(x){
   if(is.brob(x)){
@@ -57,6 +63,11 @@ setAs("brobmat", "matrix", function(from){
 `brobmat_to_brob` <- function(x){ brob(c(getX(x)),c(getP(x))) }
 
 setMethod("as.matrix",signature(x="brobmat"),function(x){as(x,"matrix")})
+setGeneric("nrow")
+setGeneric("ncol")
+setMethod("nrow",signature(x="brobmat"),function(x){nrow(getX(x))})
+setMethod("ncol",signature(x="brobmat"),function(x){ncol(getX(x))})
+
 
 `.brobmat.print` <- function(x, digits=5){
     out <- getX(x)
@@ -91,3 +102,149 @@ setReplaceMethod("[",signature(x="brobmat"),
                    jj.pos[i,j] <- getP(value)
                    return(brobmat(x=jj.x,positive=jj.pos))
                  } )
+
+
+
+
+setMethod("Math", "brobmat",
+          function(x){
+            switch(.Generic,
+                   abs    = brobmat(getX(x)),
+                   log    = {
+                     out <- getX(x)
+                     out[!getP(x)] <- NaN
+                     out  # numeric matrix
+                   },
+                   log10  = {
+                     out <- getX(x)
+                     out[!getP(x)] <- NaN
+                     out/log(10)  # numeric matrix
+
+                   },
+                   log2 = {
+                     out <- getX(x)
+                     out[!getP(x)] <- NaN
+                     out/log(2)  # numeric matrix
+                   },
+                   exp    =,
+                   cosh   =,
+                   sinh   =,
+                   acos   =,
+                   acosh  =,
+                   asin   =,
+                   asinh  =,
+                   atan   =,
+                   atanh  =,
+                   cos    =,
+                   sin    =,
+                   tan    =,
+                   tanh   =,
+                   trunc  =,
+                   lgamma =,
+                   cumsum =,
+                   gamma  =,
+                   ceiling=,
+                   floor  =,
+                   stop(paste(.Generic, "not implemented on brobmats"))
+                     )
+          } )
+
+setMethod("Arith",signature(e1 = "brobmat", e2="missing"),
+          function(e1,e2){
+            switch(.Generic,
+                   "+" = e1,
+                   "-" = newbrobmat(getX(e1),positive=!getP(e1)),
+                   stop(paste("Unary operator", .Generic,
+                              "not allowed on brobmats"))
+                   )
+          } )
+
+
+"brobmat.arith" <- function(e1,e2){
+  switch(.Generic,
+         "+" = brobmat.add  (e1,  e2),
+         "-" = brobmat.add  (e1, -e2),
+         "*" = brobmat.mult (e1, e2),
+         "/" = brobmat.mult (e1, brobmat.inverse(e2)),
+         "^" = brobmat.power(e1, e2),
+         stop(paste("binary operator \"", .Generic, "\" not defined for Brobdingnagian numbers"))
+         ) }
+
+setMethod("Arith", signature(e1 = "brobmat", e2="brob"   ), brobmat.arith)
+setMethod("Arith", signature(e1 = "brob"   , e2="brobmat"), brobmat.arith)
+setMethod("Arith", signature(e1 = "brobmat", e2="ANY"    ), brobmat.arith)
+setMethod("Arith", signature(e1 = "ANY"    , e2="brobmat"), brobmat.arith)
+setMethod("Arith", signature(e1 = "brobmat", e2="brobmat"), brobmat.arith)
+
+`getat` <- function(e1,e2=e1){
+    if(length(e1)>=length(e2)){
+        return(attributes(getX(e1)))
+    } else {
+        return(attributes(getX(e2)))
+    }
+}
+
+`brobmat.add` <- function(e1,e2){
+    out <- as.brob(e1) + as.brob(e2)
+    jj <- getX(out)
+    attributes(jj) <- getat(e1,e2)
+    return(newbrobmat(jj,getP(out)))
+}
+
+`brobmat.mult` <- function(e1,e2){
+    out <- as.brob(e1) * as.brob(e2)
+    jj <- getX(out)
+    attributes(jj) <- getat(e1,e2)
+    return(newbrobmat(jj,getP(out)))
+}
+
+`brobmat.inverse` <- function(e1){
+    if(is.brobmat(e1)){
+        out <- 1/as.brob(e1)
+        jj <- getX(out)
+        attributes(jj) <- getat(e1)
+        return(newbrobmat(jj,getP(out)))
+    } else {
+        return(1/e1)
+    }
+}
+
+`brobmat.power` <- function(e1,e2){
+    out <- as.brob(e1) ^ as.brob(e2)
+    jj <- getX(out)
+    attributes(jj) <- getat(e1,e2)
+    return(newbrobmat(jj,getP(out)))
+}
+
+"brobmat.equal" <- function(e1,e2){
+    out <- as.brob(e1) == as.brob(e2)
+    attributes(out) <- getat(e1,e2)
+    return(out)
+}
+
+"brobmat.greater" <- function(e1,e2){
+    out <- as.brob(e1) > as.brob(e2)
+    attributes(out) <- getat(e1,e2)
+    return(out)
+}
+
+"brobmat.compare" <- function(e1,e2){
+   if( (length(e1) == 0) | (length(e2)==0)) {
+       return(logical(0))
+   }
+
+  switch(.Generic,
+         "==" =  brobmat.equal(e1,e2),
+         "!=" = !brobmat.equal(e1,e2),
+         ">"  =  brobmat.greater(e1,e2),
+         "<"  = !brobmat.greater(e1,e2) & !brobmat.equal(e1,e2),
+         ">=" =  brobmat.greater(e1,e2) |  brobmat.equal(e1,e2),
+         "<=" = !brobmat.greater(e1,e2) |  brobmat.equal(e1,e2),
+         stop(paste(.Generic, "not supported for brobmats numbers"))
+         )
+}
+
+setMethod("Compare", signature(e1="brobmat", e2="ANY"    ), brobmat.compare)
+setMethod("Compare", signature(e1="ANY"    , e2="brobmat"), brobmat.compare)
+setMethod("Compare", signature(e1="brobmat", e2="brobmat"), brobmat.compare)
+
