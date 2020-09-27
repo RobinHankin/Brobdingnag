8bf76789e6b9b3bbabadf3ddbdaa373c357aac57
RobinHankin <hankin.robin@gmail.com>
2020-09-27 13:12:47 +1300

    move integration to a vignette

--- inst/testcase.R
+++
@@ -1,9 +0,0 @@
-library("Brobdingnag")
-library("cubature")
-
-f.b <- function(x) {
-  x <- as.brob(x[1, ])
- as.matrix( brobmat(-x^2 + 4, ncol = length(x)))
-}
-
-print(cubature::hcubature(f = f.b, lowerLimit = -2, upperLimit = 2, vectorInterface = TRUE))
