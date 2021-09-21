297800db99f240e4e602555c249e7e76732038bd
Robin Hankin <hankin.robin@gmail.com>
2021-09-21 14:51:27 +1200

    sticker

---
+++ inst/brobdingnag_stickermaker.R
@@ -0,0 +1,4 @@
+library("hexSticker")
+sticker("brobmap_7733ff.png", package="Brobdingnag", p_size=20, s_x=0.8, s_y=0.74,
+s_width=1.1,asp=sqrt(3)/2, white_around_sticker=TRUE, h_fill="#7733FF",
+h_color="#000000", filename="brobdingnag.png")
